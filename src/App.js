ee45674d04a6e0fa2b7be8901d1dc4f2f4c70b9a
Lokeshjana <janalokesh96@gmail.com>
2024-05-08 00:49:35 +0530

    changes for better ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
-
-const API_KEY = '1635890035cbba097fd5c26c8ea672a1';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AOS from "aos";
+import 'aos/dist/aos.css';
+const API_KEY = "1635890035cbba097fd5c26c8ea672a1";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
 function App() {
   const [forecastData, setForecastData] = useState([]);
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   const NoDataFound = () => {
     return (
@@ -15,18 +20,20 @@ function App() {
         <p>Sorry, no weather data is available for the selected city.</p>
       </div>
     );
-  }
+  };
 
   const fetchData = async (city) => {
     try {
-      const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+      const response = await fetch(
+        `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
       if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
+        throw new Error("Failed to fetch weather data");
       }
       const data = await response.json();
       const currentDate = new Date();
       const nextFiveDays = [];
-      data.list.forEach(item => {
+      data.list.forEach((item) => {
         const itemDate = new Date(item.dt * 1000);
         if (itemDate.getDate() !== currentDate.getDate()) {
           nextFiveDays.push(item);
@@ -37,47 +44,72 @@ function App() {
       });
       setForecastData(nextFiveDays);
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error("Error fetching weather data:", error);
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       fetchData(city);
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error("Error fetching weather data:", error);
     }
   };
 
   return (
     <div className="App">
-      <h1>5-Day Weather Forecast  {city && forecastData?.length > 0 && "- " + city}</h1>
+      <h1>
+        5-Day Weather Forecast {city && forecastData?.length > 0 && "- " + city}
+      </h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={city}
           onChange={(e) => {
-            setCity(e.target.value)
-            setForecastData([])
+            setCity(e.target.value);
+            setForecastData([]);
           }}
           placeholder="Enter city name"
+          className="input_btn"
         />
-        <button type="submit">Search</button>
+        <button type="submit" className="search_btn">
+          Search
+        </button>
       </form>
       <div className="forecast-container">
-        {forecastData?.length > 0 ? forecastData.map((day, index) => (
-          <div className="weather-card" key={index}>
-          <div className="weather-content">
-            <h2>{new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' })}</h2>
-            <p>{new Date(day.dt * 1000).toLocaleString()}</p>
-            <p>Temperature: {day.main.temp}°C</p>
-            <p>Description: {day.weather[0].description}</p>
+        {forecastData?.length > 0 &&
+          forecastData.map((day, index) => (
+            <div data-aos="fade-up">
+              <div className="weather-card" key={index}>
+                <div className="weather-content">
+                  <img
+                    className="sun"
+                    alt="sun_image"
+                    src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"
+                  />
+                  <h2>
+                    {new Date(day.dt * 1000).toLocaleDateString("en-US", {
+                      weekday: "long",
+                    })}
+                  </h2>
+                  <p>{new Date(day.dt * 1000).toLocaleString()}</p>
+                  <p>Temperature: {day.main.temp}°C</p>
+                  <p>Description: {day.weather[0].description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        {city === "" ? (
+          <div className="no-data-found">
+            <h3>
+              Please enter city name to get weather forecast data for next 5
+              days{" "}
+            </h3>
           </div>
-          <div className="sun"></div>
-        </div>
-        )) : <NoDataFound />
-        }
+        ) : (
+          city && forecastData?.length === 0 && <NoDataFound />
+        )}
       </div>
     </div>
   );
