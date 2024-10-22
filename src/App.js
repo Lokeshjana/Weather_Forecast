8d7169d3739a2374d8ee11672af5bac83e052af4
Lokeshjana <janalokesh96@gmail.com>
2024-10-22 23:09:52 +0530

    Weather_Forecast updated changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import AOS from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
+import loader from "../src/loading-gif.gif";
 
 const API_KEY = "1635890035cbba097fd5c26c8ea672a1";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
@@ -11,11 +12,16 @@ function App() {
   const [city, setCity] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [activeTab, setActiveTab] = useState("individual");
 
   useEffect(() => {
     AOS.init();
   }, []);
+
+  const formatDate = (itemDate) => {
+    return `${String(itemDate.getDate()).padStart(2, "0")}/${String(
+      itemDate.getMonth() + 1
+    ).padStart(2, "0")}/${itemDate.getFullYear()}`;
+  };
 
   const fetchData = async (city) => {
     setLoading(true);
@@ -28,15 +34,23 @@ function App() {
         throw new Error("Failed to fetch weather data");
       }
       const data = await response.json();
-      const currentDate = new Date();
       const nextFiveDays = [];
+      let currentDate = new Date();
       data.list.forEach((item) => {
         const itemDate = new Date(item.dt * 1000);
         if (itemDate.getDate() !== currentDate.getDate()) {
-          nextFiveDays.push(item);
-          if (nextFiveDays.length === 5) {
-            return nextFiveDays;
-          }
+          nextFiveDays.push({
+            id: formatDate(itemDate),
+            date: formatDate(itemDate),
+            temp: item.main.temp,
+            minTemp: item.main.temp_min,
+            maxTemp: item.main.temp_max,
+            pressure: item.main.pressure,
+            humidity: item.main.humidity,
+          });
+
+          currentDate = itemDate;
+          if (nextFiveDays?.length === 5) return;
         }
       });
       setForecastData(nextFiveDays);
@@ -48,112 +62,81 @@ function App() {
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (city) {
-      fetchData(city);
-    }
+  const handleSubmit = () => {
+    if (city) fetchData(city);
   };
 
-  const groupByDay = (data) => {
-    const groupedData = data.reduce((acc, item) => {
-      const date = new Date(item.dt * 1000).toLocaleDateString("en-US");
-      if (!acc[date]) {
-        acc[date] = [];
-      }
-      acc[date].push(item);
-      return acc;
-    }, {});
-    return Object.entries(groupedData);
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+    setForecastData([]);
+    setError("");
   };
 
   return (
     <div className="App">
-      <h1>
-        5-Day Weather Forecast {city && forecastData.length > 0 && `- ${city}`}
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => {
-            setCity(e.target.value);
-            setForecastData([]);
-            setError("");
-          }}
-          placeholder="Enter city name"
-          className="input_btn"
-        />
-        <button type="submit" className="search_btn">
-          Search
-        </button>
-      </form>
-      <div className="tabs">
-        <button
-          className={`tab ${activeTab === "individual" ? "active" : ""}`}
-          onClick={() => setActiveTab("individual")}
-        >
-          Individual
-        </button>
-        <button
-          className={`tab ${activeTab === "grouped" ? "active" : ""}`}
-          onClick={() => setActiveTab("grouped")}
-        >
-          Grouped by Day
-        </button>
+      <div className="column ">
+        <h1>Weather in your city</h1>
+        <div className="heading-input">
+          <input
+            type="text"
+            value={city}
+            onChange={handleInputChange}
+            placeholder="Enter city name"
+            className="input_btn"
+          />
+          <button type="submit" className="search_btn" onClick={handleSubmit}>
+            <span>&#x3f;</span> Search
+          </button>
+          {loading && <img src={loader} alt="loading" />}
+        </div>
       </div>
       <div className="forecast-container">
-        {loading && <p>Loading...</p>}
         {error && (
           <div className="no-data-found">
             <h3>No Data Found</h3>
             <p>{error}</p>
           </div>
         )}
-        {forecastData.length > 0 && activeTab === "individual" && (
-          forecastData.map((day, index) => (
-            <div key={index} data-aos="fade-up" className="weather-card">
-              <div className="weather-content">
-                <img
-                  className="sun"
-                  alt="sun_image"
-                  src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"
-                />
-                <h2>
-                  {new Date(day.dt * 1000).toLocaleDateString("en-US", {
-                    weekday: "long",
-                  })}
-                </h2>
-                <p>{new Date(day.dt * 1000).toLocaleString()}</p>
-                <p>Temperature: {day.main.temp}°C</p>
-                <p>Description: {day.weather[0].description}</p>
+        {forecastData.length > 0
+          ? forecastData.map((day) => (
+              <div key={day.id} data-aos="fade-up" className="weather-card">
+                <table className="weather-table">
+                  <tbody>
+                    <tr className="date-row">
+                      <td colSpan="2"> Date: {day.date} </td>
+                    </tr>
+                    <tr>
+                      <td colSpan="2">Temperature: {day.temp}°C</td>
+                    </tr>
+                    <tr>
+                      <th>Min</th>
+                      <th>Max</th>
+                    </tr>
+                    <tr>
+                      <td>{day.minTemp}°C</td>
+                      <td>{day.maxTemp}°C</td>
+                    </tr>
+                    <tr>
+                      <th>Pressure</th>
+                      <td>{day.pressure} hPa</td>
+                    </tr>
+                    <tr>
+                      <th>Humidity</th>
+                      <td>{day.humidity}%</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-            </div>
-          ))
-        )}
-        {forecastData.length > 0 && activeTab === "grouped" && (
-          groupByDay(forecastData).map(([date, items], index) => (
-            <div key={index} data-aos="fade-up" className="weather-card full-width">
-              <div className="weather-content">
-                <h2>{date + ' - ' + new Date(items[0]?.dt * 1000).toLocaleDateString("en-US", {
-                  weekday: "long",
-                })}</h2>
-                {items.map((item, idx) => (
-                  <div key={idx} className="weather-row">
-                    <p>{new Date(item.dt * 1000).toLocaleTimeString()}</p>
-                    <p>Temperature: {item.main.temp}°C</p>
-                    <p>Description: {item.weather[0].description}</p>
-                  </div>
-                ))}
+            ))
+          : !loading &&
+            !error && (
+              <div className="no-data-found">
+                <h3>
+                  Please enter a city name to get the weather forecast data for
+                  the next 5 days
+                </h3>
               </div>
-            </div>
-          ))
-        )}
-        {!loading && !error && forecastData.length === 0 && (
-          <div className="no-data-found">
-            <h3>Please enter a city name to get the weather forecast data for the next 5 days</h3>
-          </div>
-        )}
+            )}
       </div>
     </div>
   );
